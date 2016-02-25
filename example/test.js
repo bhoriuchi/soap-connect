995d51b231ce82266826265a3b4ecf72cbcc58a8
Branden Horiuchi <bhoriuchi@gmail.com>
2016-02-24 16:04:06 -0800

    saving

--- example/test.js
+++ example/test.js
@@ -1,3 +1,6 @@
-/**
- * New node file
- */
+var client = require('../lib');
+
+
+client.wsdl.parse('https://vcenter01.home.local/sdk/vimService.wsdl', {ignoreSSL: true}).then(function(data) {
+	console.log(JSON.stringify(data, null, '  '));
+});
