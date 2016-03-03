2377b39eb911a365c96104e231afb0df4bd7ad3c
Branden Horiuchi <bhoriuchi@gmail.com>
2016-03-03 15:22:19 -0800

    saving

--- example/test.js
+++ example/test.js
@@ -1,6 +1,33 @@
 var client = require('../lib');
 var cred   = require('../credentials');
+var util   = require('util');
+var dotp   = require('dotprune');
+var _ = require('lodash');
 
-client.wsdl.parse(cred.wsdl, {ignoreSSL: true}).then(function(data) {
-	console.log(JSON.stringify(data, null, '  '));
+cred.wsdl = 'https://pvvapzz030.la.frd.directv.com/sdk/vimService.wsdl';
+
+
+client.wsdl(cred.wsdl).then(function(wsdl) {
+	
+	
+	console.log(JSON.stringify(wsdl.schemas['urn:vim25'].DynamicData, null, '  '));
+	
+	//console.log(_.keys(data));
+	
+	//console.log(JSON.stringify(data, null, '  '));
+	
+	//var de = data.documentElement;
+	
+	//onsole.log(de._nsMap);
+	
+	//var wsdlNS = data.getAttribute('xmlns');
+	//console.log(wsdlNS);
+	//var schemaNS = de.getElementsByTagNameNS(wsdlNS, 'types');
+	
+	//console.log(wsdlNS);
+	//console.log(_.keys(schemaNS));
+	
+	//var out = data.documentElement.getElementsByTagNameNS('http://www.w3.org/2001/XMLSchema', 'include');
+	
+	//console.log(out['0'].attributes, out['1'].attributes, out['2'].attributes);
 });
