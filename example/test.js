cd7bdffd04bb5df4acf8669497b0bd0206ed7e13
Branden Horiuchi <bhoriuchi@gmail.com>
2016-03-07 15:08:22 -0800

    saving

--- example/test.js
+++ example/test.js
@@ -4,12 +4,13 @@ var dotp   = require('dotprune');
 var _ = require('lodash');
 
 
-client.wsdl('https://pvvapzz030.la.frd.directv.com/sdk/vimService.wsdl').then(function(wsdl) {
+client.wsdl('https://vcenter01.home.local/sdk/vimService.wsdl').then(function(wsdl) {
 	
 	
-	console.log(JSON.stringify(wsdl.schemas['urn:vim25'].DynamicData, null, '  '));
-	console.log(JSON.stringify(wsdl.schemas['urn:vim25'].ConfigSpecOperation, null, '  '));
-	
+	//console.log(JSON.stringify(wsdl.metadata.schemas['urn:vim25'].DynamicData, null, '  '));
+	//console.log(JSON.stringify(wsdl.metadata.schemas['urn:vim25'].ConfigSpecOperation, null, '  '));
+	//console.log(JSON.stringify(wsdl.metadata.schemas['urn:vim25'].ManagedObjectReference, null, '  '));	
+	console.log(JSON.stringify(wsdl.metadata.operations['urn:vim25'].UpdateInternetScsiAuthenticationProperties, null, '  '));
 	
 	//console.log(_.keys(data));
 	
