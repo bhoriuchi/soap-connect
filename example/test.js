f724884f20c45c4d68553ad31318a1bb457a69e4
Branden Horiuchi <bhoriuchi@gmail.com>
2016-03-18 15:49:28 -0700

    completed metadata construction adding type validators
    * metadata is now created from processed wsdl and stored in
      localstorage as a caching mechanism
    * type validation started

--- example/test.js
+++ example/test.js
@@ -1,39 +1,13 @@
 var client = require('../lib');
 var util   = require('util');
-var dotp   = require('dotprune');
 var _ = require('lodash');
 var fs = require('fs');
 
-client.wsdl('https://pvvapzz030.la.frd.directv.com/sdk/vimService.wsdl').then(function(wsdl) {
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+client.wsdl('https://pvvapzz030.la.frd.directv.com/sdk/vimService.wsdl', {cache: true}).then(function(wsdl) {
+	fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
 	
-	var jstr = JSON.stringify(wsdl, null, '  ');
-	
-	//console.log(jstr);
-	
-	fs.writeFileSync('out.txt', jstr);
-	
-	
-	//console.log(JSON.stringify(wsdl.metadata.schemas['urn:vim25'].DynamicData, null, '  '));
-	//console.log(JSON.stringify(wsdl.metadata.schemas['urn:vim25'].ConfigSpecOperation, null, '  '));
-	//console.log(JSON.stringify(wsdl.metadata.schemas['urn:vim25'].ManagedObjectReference, null, '  '));	
-	//console.log(JSON.stringify(wsdl.metadata.operations['urn:vim25'].UpdateInternetScsiAuthenticationProperties, null, '  '));
-	
-	//console.log(_.keys(data));
-	
-	//console.log(JSON.stringify(data, null, '  '));
-	
-	//var de = data.documentElement;
-	
-	//onsole.log(de._nsMap);
-	
-	//var wsdlNS = data.getAttribute('xmlns');
-	//console.log(wsdlNS);
-	//var schemaNS = de.getElementsByTagNameNS(wsdlNS, 'types');
-	
-	//console.log(wsdlNS);
-	//console.log(_.keys(schemaNS));
-	
-	//var out = data.documentElement.getElementsByTagNameNS('http://www.w3.org/2001/XMLSchema', 'include');
-	
-	//console.log(out['0'].attributes, out['1'].attributes, out['2'].attributes);
+	console.log(JSON.stringify(wsdl.vim25.ArrayUpdateSpec(), null, '  '));
+
 });
