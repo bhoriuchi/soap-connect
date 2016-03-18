dde399d21a789b6b172e4382ee9e4f0b11a22ebf
Branden Horiuchi <bhoriuchi@gmail.com>
2016-03-18 16:13:33 -0700

    update to w3 types

--- example/test.js
+++ example/test.js
@@ -5,9 +5,9 @@ var fs = require('fs');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-client.wsdl('https://pvvapzz030.la.frd.directv.com/sdk/vimService.wsdl', {cache: true}).then(function(wsdl) {
-	fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
-	
-	console.log(JSON.stringify(wsdl.vim25.ArrayUpdateSpec(), null, '  '));
+client.wsdl('https://vcenter01/sdk/vimService.wsdl', {cache: true}).then(function(wsdl) {
+	//fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
+	//console.log(wsdl._meta.schema['http://www.w3.org/2001/XMLSchema']);
+	console.log(JSON.stringify(wsdl.vim25.DynamicProperty(), null, '  '));
 
 });
