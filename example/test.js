9bf729a3268736893913cd3db6d4e3f1bddbe267
Branden Horiuchi <bhoriuchi@gmail.com>
2016-03-21 15:05:49 -0700

    sending soap requests

--- example/test.js
+++ example/test.js
@@ -2,12 +2,30 @@ var client = require('../lib');
 var util   = require('util');
 var _ = require('lodash');
 var fs = require('fs');
+var cred = require('../credentials');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-client.wsdl('https://vcenter01/sdk/vimService.wsdl', {cache: true}).then(function(wsdl) {
-	//fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
-	//console.log(wsdl._meta.schema['http://www.w3.org/2001/XMLSchema']);
-	console.log(JSON.stringify(wsdl.vim25.DynamicProperty(), null, '  '));
+client.wsdl(cred.wsdl, {cache: true}).then(function(wsdl) {
+	fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
 
+	var port = wsdl.vim25Service.VimService.VimPort;
+	
+	//console.log(_.keys(port).sort());
+	port.RetrieveServiceContent({
+		_this: 'ServiceInstance'
+	});
+	
+	/*
+	console.log(JSON.stringify(wsdl.vim25.KeyAnyValue(), null, '  '));
+	console.log(JSON.stringify(wsdl.vim25.KeyAnyValue({
+		dynamicProperty: {
+			val: '100',
+			//name: 'hi'
+		},
+		dynamicType: 1,
+		key: 'imakey',
+		value: 100
+	}), null, '  '));
+	 */
 });
