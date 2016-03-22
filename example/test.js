fb2f9ca04b8955a9853abf39ad0259337648a532
Branden Horiuchi <bhoriuchi@gmail.com>
2016-03-22 15:02:48 -0700

    trying different approach to wsdl processing

--- example/test.js
+++ example/test.js
@@ -6,15 +6,19 @@ var cred = require('../credentials');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-client.wsdl(cred.wsdl, {cache: true}).then(function(wsdl) {
-	fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
+client.client(cred.wsdl, {cache: false}).then(function(wsdl) {
+	//console.log(wsdl._meta);
+	
+	//fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
 
+	/*
 	var port = wsdl.vim25Service.VimService.VimPort;
 	
 	//console.log(_.keys(port).sort());
 	port.RetrieveServiceContent({
 		_this: 'ServiceInstance'
 	});
+	*/
 	
 	/*
 	console.log(JSON.stringify(wsdl.vim25.KeyAnyValue(), null, '  '));
