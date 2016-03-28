bf7624a53853b8ecb62b31cfcae1051a868b6b23
Branden Horiuchi <bhoriuchi@gmail.com>
2016-03-28 15:56:41 -0700

    starting request code

--- example/test.js
+++ example/test.js
@@ -10,33 +10,52 @@ var cred = require('../credentials');
 //module.exports = {wsdl: 'http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL'};
 //module.exports = {wsdl: 'http://www.thomas-bayer.com/axis2/services/BLZService?wsdl'};
 
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 client.client(cred.wsdl, {cache: true}).then(function(wsdl) {
 	//console.log(wsdl._meta);
-	
-	fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
+	//console.log(wsdl);
+	//fs.writeFileSync('out.txt', JSON.stringify(wsdl._meta, null, '  '));
 
-	/*
-	var port = wsdl.vim25Service.VimService.VimPort;
+	
+	var port = wsdl.services.VimService.VimPort;
 	
 	//console.log(_.keys(port).sort());
 	port.RetrieveServiceContent({
 		_this: 'ServiceInstance'
 	});
-	*/
+	
 	
 	/*
+	console.log(JSON.stringify(wsdl.vim25.ObjectSpec({
+		obj: 'moref',
+		selectSet: [{
+			name: 't'
+		}]
+	}), null, '  '));
+	*/
+	
+	
+	/*
+	console.log(JSON.stringify(wsdl.types.vim25.ObjectSpec({
+		obj: 'moref',
+		selectSet: [{
+			$$type: 'vim25:TraversalSpec',
+			path: '/blah/blah',
+			type: 'Folder'
+		}]
+	}), null, '  '));
+	*/
+	
+	
+	/*
+	
 	console.log(JSON.stringify(wsdl.vim25.KeyAnyValue(), null, '  '));
 	console.log(JSON.stringify(wsdl.vim25.KeyAnyValue({
-		dynamicProperty: {
-			val: '100',
-			//name: 'hi'
-		},
+		dynamicProperty: [{val: '100', name: 'hi'}, {val: 100, name: 'int'}],
 		dynamicType: 1,
 		key: 'imakey',
 		value: 100
-	}), null, '  '));
-	 */
+	}), null, '  '));*/
+	
 });
